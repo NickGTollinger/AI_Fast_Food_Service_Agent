5dd9bea47b4c1d5f9477d5337e92ed0befafc597
Deonte1212 <deontehewell12@gmail.com>
2025-03-28 19:54:35 -0500

    test

--- services/cohereService.js
+++ services/cohereService.js
@@ -4,14 +4,14 @@ dotenv.config();
 
 const sessionOrders = {};
 
-const connectDB = require('../db');
-let db;
-
-(async() => {
-    db = await connectDB();
-    const collection = db.collection("menu_items");
-    menuItems = await collection.find({}.toArray());
-})();
+//This is the placeholder menu for testing purposes. The full menu of items from MongoDB will replace these.
+const menuItems = [
+  { name: "3 Finger Combo", price: 7.99 },
+  { name: "Caniac Combo", price: 10.99 },
+  { name: "Box Combo", price: 8.99 },
+  { name: "Texas Toast", price: 1.50 },
+  { name: "Lemonade", price: 2.49 }
+];
 
 //Adjust to remove case sensitivity and punctuation where needed
 const normalize = (text) => text.toLowerCase().replace(/[^a-z0-9\s]/g, '');
